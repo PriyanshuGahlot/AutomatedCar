3a95a67eb115d8804ebade06de665ee70a098fca
PriyanshuGahlot <75079838+PriyanshuGahlot@users.noreply.github.com>
2023-12-30 00:37:51 +0530

    Update AutomatedCar.ino

--- AutomatedCar.ino
+++ AutomatedCar.ino
@@ -134,12 +134,4 @@ void Stop(){
   digitalWrite(motorF2,LOW);
   digitalWrite(motorB1,LOW);
   digitalWrite(motorB2,LOW);
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
